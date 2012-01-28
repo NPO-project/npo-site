3b273fab7b44a59e2b1f1b2e33934ef0443104ac
Marlin Cremers <marlinc@mms-projects.net>
2012-01-28 23:00:58 +0100

    Testing Travis using a official channel

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-wget http://pear.php.net/go-pear.phar
-php go-pear.phar
-pear channel-discover zend.googlecode.com/svn
-pear install zend/zend
+pyrus.phar . channel-discover packages.zendframework.com
+pyrus.phar . install zf2/Zend_Framework#Standard
 phpenv rehash
