7e3f429a3fbef499242273300289ecbde9af81b9
Marlin Cremers <marlinc@mms-projects.net>
2012-01-28 23:02:45 +0100

    Fixed the wrong Pyrus path

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pyrus.phar . channel-discover packages.zendframework.com
-pyrus.phar . install zf2/Zend_Framework#Standard
+pyrus channel-discover packages.zendframework.com
+pyrus install zf2/Zend_Framework#Standard
 phpenv rehash
