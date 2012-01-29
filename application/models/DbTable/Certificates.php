3a6f822f1603008b9e9a66146fd7612110f237dc
Marlin Cremers <marlinc@mms-projects.net>
2012-01-29 20:44:25 +0100

    Fixed a mistake in the certificate DbTable

--- application/models/DbTable/Certificates.php
+++ application/models/DbTable/Certificates.php
@@ -3,8 +3,8 @@
 class Application_Model_DbTable_Certificates extends Zend_Db_Table_Abstract
 {
 
-    protected $_nameSyntax = 'nl_%s_certificates';
-
+    protected $_nameSyntax = 'npo_%s_certificates';
+    
     public function setWorld($world)
     {
         $this->_name = sprintf($this->_nameSyntax, $world);
