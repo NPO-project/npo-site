e12f48c0521f903c229e19ea14108a39af0cecc9
Marlin Cremers <marlinc@mms-projects.net>
2012-01-29 19:14:26 +0100

    The page title is now set in the controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,7 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $this->view->headTitle('Tribal Wars NPO-Manifest (draft)');
     }
 
     public function indexAction()
