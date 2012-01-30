0ba489d15c4a4eb36febee99cd4cd0d8ff645ece
Marlin Cremers <marlinc@mms-projects.net>
2012-01-30 19:41:02 +0100

    Renamed the 'alt' controller test and modified the index test

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -5,13 +5,20 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->bootstrap = new Zend_Application(
+            APPLICATION_ENV,
+            APPLICATION_PATH . '/configs/application.ini'
+        );
         parent::setUp();
     }
 
     public function testIndexAction()
     {
-        $params = array('action' => 'index', 'controller' => 'Index', 'module' => 'default');
+        $params = array(
+            'action' => 'index', 
+            'controller' => 'Registration', 
+            'module' => 'default'
+        );
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
         $this->dispatch($url);
