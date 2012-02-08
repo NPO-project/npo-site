8e5b8098e84452c45743a581f42a56c68d9c5cdc
dennisdegryse <dennisdegryse@gmail.com>
2012-02-08 11:29:14 +0100

    Added environment variables to test setup

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,3 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+export DB_NAME=npo
+export DB_PREFIX=npo_
+export DB_USER=postgres
+export DB_PASS=npodev
+
+export SERVER_NAME=test.npo.tribalwars
+export SERVER_ADMIN=npo.tribalwars@gmail.com
+
+export SMTP_HOST=smtp.gmail.com
+export SMTP_USER=npo.tribalwars@gmail.com
+export SMTP_PASS=npodev12
+export SMTP_FROM=npo.tribalwars@gmail.com
+
+export HTTPD_USER=apache
+
+export ENV=development
+
 make
 #sh tests/travis/setup-zend.sh
